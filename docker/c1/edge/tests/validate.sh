5cacdb41398e54e0f4a71dd35a03f66487c9efee
trosvald <eko.pur25@gmail.com>
2026-09-02 16:59:49 +0700

    fix c1 edge recovery runtime assets

--- docker/c1/edge/tests/validate.sh
+++ docker/c1/edge/tests/validate.sh
@@ -23,6 +23,7 @@ jq -e '
   and ([.services | to_entries[] | select(.value.networks.c1_edge? != null) | .key] == ["haproxy"])
   and ([.services | to_entries[] | select(.value.networks.forgejo_frontend? != null) | .key] == ["haproxy"])
   and .services.haproxy.cap_add == ["NET_BIND_SERVICE"]
+  and .services["vector-init"].cap_add == ["CHOWN","FOWNER"]
   and .services.haproxy.ulimits.nofile == {"soft":65536,"hard":65536}
   and (.services.haproxy.mem_limit | tonumber) == 1073741824
   and (.services.haproxy.memswap_limit | tonumber) == 1073741824
